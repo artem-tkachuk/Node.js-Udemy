a3d7e464b04560586644f08784432a9c6f843b02
Artem Tkachuk <artemtkachuk@yahoo.com>
2019-05-31 12:31:51 -0700

    Lecture 16

--- play.js
+++ play.js
@@ -1,10 +1,14 @@
-var name = 'Max';
-var age = 29;
-console.log(name);
+const name = 'Artem';
+const hasHobbies = true;
 
-var hasHobbies = true;
+let age = 18;
+console.log(age);
+age = 19;
 
-function summarizeUser(userName, userAge, userHasHobby) {
+//name = 'Art';     Doesn't work because const value cannot be changed
+
+const summarizeUser = (userName, userAge, userHasHobby) => {
+    //'this' behaves differently comparing with the usual function (if arrow function is used)
     return (
         'Name is '
         + userName +
@@ -13,6 +17,37 @@ function summarizeUser(userName, userAge, userHasHobby) {
         ', user has hobbies: '
         + userHasHobby
     );
-}
+};
 
 console.log(summarizeUser(name, age, hasHobbies));
+
+const add = (a, b) => a + b;
+const addOne = a => a + 1;
+const addRandom = () => Math.random() * 100;
+
+console.log(add(1,2));
+console.log(addOne(1));
+console.log(addRandom());
+
+
+const person = {
+    name: 'Artem',
+    age: 19,
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+}
+
+person.greet();
+
+
+const hobbies = ['Sports', 'Music', 555, true]; //this is a const reference ==> we can change the data it is POINTING to, but not the pointer itself
+
+for (let hobby of hobbies) {
+    console.log(hobby);
+}
+
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+
+hobbies.push('Programming');
+console.log(hobbies);
